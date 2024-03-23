1f0ac8c597496865ab0e13904b02cf8029c2939e
Andrei Lebedev <a.lebedev234@gmail.com>
2024-03-23 20:54:53 +0300

    Adding README.md files

--- nltk_vs_regexp/token_.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nf ='test_tokrus.txt'
-f = open(nf,"r")
-sentences = nltk.sent_tokenize(f.read()  , language="russian")
-i = 1
-for sentence in sentences: 
-    print(i, '  ',sentence)
-    i += 1
